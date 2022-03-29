dc2c60861ae87f54f1cd9153f5b6805fc9329665
Louise <wddtechnologies@users.noreply.github.com>
2022-03-29 16:36:42 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -3,8 +3,16 @@
 
 function App() {
   return (
-    <div className="App">
-     
+    <div className="grid-container">
+     <header>
+       < a href="/">React Shopping Cart</a>
+     </header>
+     <main>
+       Product list
+     </main>
+     <footer>
+       All rights reversed
+     </footer>
     </div>
   );
 }
